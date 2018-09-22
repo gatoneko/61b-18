71f2a49eeb03baae73f62a4921589412ae16cb02
Consco <consco@Jesses-iMac.local>
2018-09-22 09:13:31 -0500

    create Node class

---
+++ proj3/src/main/java/Node.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Node {
+    private long id;
+    private double lon;
+    private double lat;
+    private String name;
+    private List<Edge> partOfEdges;
+    private List<Node> adjacentNodes;
+
+    public boolean isConnected() {
+        return !adjacentNodes.isEmpty();
+    }
+
+    public Node(long id, double lon, double lat) {
+        this.id = id;
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
